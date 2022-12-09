f8f11ba90864c2fa17ac4cfa170449ba649f3095
TorokDan <dani.torok8@gmail.com>
2022-12-09 12:02:59 +0100

    Initial commit

---
+++ src/test/java/com/torokdan/travellog/TravellogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torokdan.travellog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
