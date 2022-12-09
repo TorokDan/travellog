ac7f979af524e198290bb41a0db9e3de154e693f
TorokDan <dani.torok8@gmail.com>
2022-12-09 21:10:02 +0100

    refactor(): unused imports

--- src/main/java/com/torokdan/travellog/modell/dto/AppUserRequestDto.java
+++ src/main/java/com/torokdan/travellog/modell/dto/AppUserRequestDto.java
@@ -1,8 +1,5 @@
 package com.torokdan.travellog.modell.dto;
 
-import com.torokdan.travellog.modell.Role;
-import java.util.List;
-
 public record AppUserRequestDto(String name, String email, String password) {
 
 }
